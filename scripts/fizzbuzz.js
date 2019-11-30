a1a64ec8dc7a63d4daceb6d221d07fa0f4d7fa7b
Dzintars Magons <dzintars.magons@gmail.com>
2019-11-30 23:19:35 +0200

    added min and max

--- scripts/fizzbuzz.js
+++ scripts/fizzbuzz.js
@@ -4,42 +4,60 @@
    document.body.appendChild(mainElement);
    
    
-   function addHundredDivs (){ 
-       for (let i = 1; i <= 100; i++){
+   function addHundredDivs (min, max){ 
+       for (let i = min; i <= max; i++){
            let divElement = document.createElement("div");
            let paragraphElement = document.createElement("p");
            mainElement.appendChild(divElement).appendChild(paragraphElement);
                 if(i % 3 === 0 && i % 5 === 0) 
                     {console.log("FizzBuzz"),
-                    divElement.setAttribute("id", "element" +i),
-                    divElement.setAttribute("class", "Fizzbuzz"),
+                    divElement.setAttribute("id", "element" +i);
+                    divElement.setAttribute("class", "Fizzbuzz");
                     paragraphElement.innerText=i + " FIZZBUZZ";
                 }
                 else if (i % 3 === 0) 
                     {console.log("Fizz"),
-                    divElement.setAttribute("id", "element" +i),
-                    divElement.setAttribute("class", "Fizz"),
+                    divElement.setAttribute("id", "element" +i);
+                    divElement.setAttribute("class", "Fizz");
                     paragraphElement.innerText=i + " FIZZ";
                 }
                 else if (i % 5 === 0) 
                     {console.log("Buzz"),
-                    divElement.setAttribute("id", "element" +i),
-                    divElement.setAttribute("class", "Buzz"),
+                    divElement.setAttribute("id", "element" +i);
+                    divElement.setAttribute("class", "Buzz");
                     paragraphElement.innerText=i + " BUZZ";
                 }
                 else 
                     {console.log(i),
-                    divElement.setAttribute("id", "element" +i),
+                    divElement.setAttribute("id", "element" +i);
+                    divElement.setAttribute("class", "number");
                     paragraphElement.innerText=i;
                 }
        }
-       
+       console.log("finished adding divs");
     }
-      addHundredDivs();
+      addHundredDivs(1,100);
 
-    // function clearDivs () {
-    //     if (document.getElementsByTagName("button").onclick)
-    //         {document.getElementsByClassName("Fizz","Buzz","Fizzbuzz").style.display= "none";}
-    //     }
+ document.getElementById("start-value").addEventListener("change", function (event) {
+     console.log("listening start");
+     deleteDivs();
+    var startVal = parseFloat(event.target.value);
+    var inputEnd = parseFloat(document.getElementById("end-value").value);
+    console.log(Number.isInteger(startVal),Number.isInteger(inputEnd));
+     addHundredDivs(startVal,inputEnd);
+ });
 
-    
+ document.getElementById("end-value").addEventListener("change", function (event) {
+    console.log("listening end");
+    deleteDivs();
+    let inputEnd = parseFloat(event.target.value);
+    console.log(inputEnd);
+    let startVal = parseFloat(document.getElementById("start-value").value);
+     addHundredDivs(startVal,inputEnd);
+});
+
+function deleteDivs () {
+    while(mainElement.firstChild){
+mainElement.firstChild.remove();
+    }
+}
